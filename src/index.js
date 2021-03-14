93b6b648b0fa192bbf990004320526ca43a0359d
wallinthefuture <wallinthefuture@mail.ru>
2021-03-14 21:31:51 +0300

    feat: add min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+exports.min = function min(array) {
+    if (array && array.length != 0) return Math.min(...array);
+    else return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array && array.length != 0) return Math.max(...array);
+    else return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array && array.length != 0)
+        return array.reduce((a, b) => a + b) / array.length;
+    else return 0;
+};
